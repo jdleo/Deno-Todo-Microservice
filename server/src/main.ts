fab9adcb8213010b1e7446cafaf9f7f546766fc2
john leonardo <j@jdleo.me>
2021-02-01 16:05:52 -0600

    enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
 import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
+import { oakCors } from 'https://deno.land/x/cors/mod.ts';
 import router from './routes/mod.ts';
 
 const app = new Application();
 
 const PORT = 3001;
 
+app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 

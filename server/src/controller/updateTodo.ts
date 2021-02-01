638211652b67f2b1a66b718f1f1ee0a722feb7c2
john leonardo <j@jdleo.me>
2021-02-01 14:26:05 -0600

    updateTodo

--- server/src/controller/updateTodo.ts
+++ server/src/controller/updateTodo.ts
@@ -2,11 +2,13 @@ import { Todo, Bson } from '../db/mod.ts';
 
 const updateTodo = async (ctx: any) => {
     try {
+        // attempt to get body from ctx
+        const body: any = await ctx.request.body();
         // attempt to get id from ctx params
         let todoId: string = ctx.params.id;
 
         // attempt to lookup by ID in mongoDB
-        const todo = await Todo.findOne({ _id: new Bson.ObjectID(todoId) });
+        const todo = await Todo.updateOne({ _id: new Bson.ObjectID(todoId) }, { $set: body });
         // send created response back
         ctx.response.body = { status: todo !== undefined, data: todo };
         ctx.response.status = 200;

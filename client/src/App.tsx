130cabca522d13a5da764c814008f7aad69951cb
john leonardo <j@jdleo.me>
2021-02-01 15:51:39 -0600

    style client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,97 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+import {
+    Typography,
+    TextField,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    makeStyles,
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
 
 function App() {
+    // state mgmt
+    const [text, setText] = useState('');
+    const [todos, setTodos] = useState(
+        Array.from({ length: 20 }, () => ({
+            created_at: 3928749872323,
+            text: 'hello this is a todo item',
+            _id: '329849238849',
+        }))
+    );
+
+    const classes = useStyles();
+
     return (
         <div className="App">
-            <Button color="primary">Hello World</Button>
+            <Typography variant="h4">Create Todo Item</Typography>
+            <TextField
+                style={{ marginTop: 20, maxWidth: '50%' }}
+                id="outlined-multiline-static"
+                label="New Todo Item"
+                value={text}
+                onChange={e => setText(e.target.value)}
+                multiline
+                rows={3}
+                variant="outlined"
+                fullWidth
+            />
+            <br />
+            <Button
+                size="large"
+                variant="contained"
+                color="primary"
+                style={{ marginTop: 20, maxWidth: '50%' }}
+                fullWidth
+            >
+                Create
+            </Button>
+            <div style={{ marginTop: 20 }}>
+                {todos.map(todo => (
+                    <Card
+                        className={classes.root}
+                        variant="outlined"
+                        style={{ marginLeft: '25%', width: '50%', marginTop: 20 }}
+                    >
+                        <CardContent>
+                            <Typography
+                                className={classes.title}
+                                color="textSecondary"
+                                gutterBottom
+                            >
+                                {new Date(todo.created_at).toLocaleString()}
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                {todo.text}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small" variant="contained" color="secondary">
+                                delete
+                            </Button>
+                        </CardActions>
+                    </Card>
+                ))}
+            </div>
         </div>
     );
 }

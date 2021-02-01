0afea820569b508357ad6c4b4392d276b4259fa8
john leonardo <j@jdleo.me>
2021-02-01 13:40:00 -0600

    register schema with mongodb

---
+++ server/src/db/mod.ts
@@ -0,0 +1,21 @@
+import { MongoClient } from 'https://deno.land/x/mongo@v0.21.0/mod.ts';
+import TodoSchema from '../model/Todo.ts';
+
+// create client
+const client = new MongoClient();
+
+// get credentials from environment
+const USER = Deno.env.get('MONGODB_ADMINUSERNAME');
+const PASS = Deno.env.get('MONGODB_ADMINPASSWORD');
+
+// connect to MongoDB
+client.connect(`mongodb://${USER}:${PASS}@localhost:27017`);
+
+// database ref
+const db_name: string = 'todos';
+const db = client.database(db_name);
+
+// register schema with Mongo
+const Todo = db.collection<TodoSchema>('todo');
+
+export { db, Todo };

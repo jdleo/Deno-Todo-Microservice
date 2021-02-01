5ba599a09d996d791e54180433d36a2a20f9b4f7
john leonardo <j@jdleo.me>
2021-02-01 12:57:55 -0600

    deno running successfully in docker service

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,10 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts';
+
+// new app instance
+const app = new Application();
+
+app.use(ctx => {
+    ctx.response.body = { hello: 'world' };
+});
+
+await app.listen({ port: 3001 });
